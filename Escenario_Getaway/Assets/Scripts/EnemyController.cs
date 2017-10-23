a737f87e3479a0bbfbeac4f8d839ac6f5dd044cf
malebel98 <malebel98@gmail.com>
2017-10-23 14:06:54 -0500

    .
    .

---
+++ Escenario_Getaway/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Slider vidaE;
+
+	void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.CompareTag("Bala"))
+        {
+            vidaE.value = vidaE.value - 0.5f;
+        }
+        if (vidaE.value == 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+	}
+}
